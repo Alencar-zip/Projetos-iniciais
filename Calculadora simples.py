ab2418b56279ae74d1bd33e82c3e91b21e662599
Alencar <alencarvitor2027@gmail.com>
2026-03-09 19:47:41 -0300

    Calculadora simples.py

---
+++ Calculadora simples.py
@@ -0,0 +1,38 @@
+print("=====Bem vindo a calculadora de simples!=====")
+
+print("")
+print("APENAS NUMEROS!")
+numA= float(input("Insira o primeiro numero: "))
+numB= float(input("Insira o segundo numero: "))
+
+operator= input("""selecione a operação a ser feita!
+(A)SOMA 
+(B)SUBTRAÇÃo
+(C)MULTIPLICAÇÃO
+(D)DIVISÃO
+
+Digite: """).upper().strip()
+
+while operator not in ["A","B","C","D"]:
+    operator= input("Digite entre as opções listadas:")
+
+if operator == "A":
+    print("Resultado: ", numA + numB)
+elif operator == "B":
+    print("Resultado: ", numA - numB)
+elif operator== "C":
+    print("Resultado: ", numA * numB)
+elif operator== "D":
+    print("Resultado: ", numA / numB)
+
+ #Codigo para repetiçao incompleto!
+repetir= input("""Deseja fazer mais uma conta? (A) Sim / (B) Não
+Digito:""").upper()
+while repetir not in ["A", "B"]:
+    repetir= input("Digite apenas as letras acima: ").upper()
+
+
+
+
+
+
